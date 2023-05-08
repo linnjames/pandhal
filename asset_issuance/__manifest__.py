7d8b4fd8e60ebc685e80c2697d3208732420bcd5
LinnJames19 <linn@catalistindia.in>
2023-05-08 19:44:12 +0530

    asset modifies

--- asset_issuance/__manifest__.py
+++ asset_issuance/__manifest__.py
@@ -2,14 +2,11 @@
     "name": "Asset Issuance",
     "summary": """Asset Issuance""",
     "sequence": 1,
-    "depends": ['base', 'account', 'contacts', 'purchase', 'stock'],
+    "depends": ['base', 'account', 'contacts', 'purchase', 'stock', 'hr', 'hr_holidays'],
     "data": [
         'security/ir.model.access.csv',
         'wizard/asset_wizard_form.xml',
         'views/asset_issuance.xml',
-
-
-
 
     ],
     "demo": [],
