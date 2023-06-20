75161d03c9fcb84fc1d5c464352953e5a995050e
r.arun@catalistindia.in <r.arun@catalistindia.in>
2023-06-20 22:43:43 +0530

    indent

--- planing_pandhal/models/planing.py
+++ planing_pandhal/models/planing.py
@@ -1,6 +1,7 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import Warning, AccessError, UserError
 from datetime import datetime, date
+import json
 
 
 class PlanPlaning(models.Model):
@@ -20,6 +21,7 @@ class PlanPlaning(models.Model):
         default='draft', string="Status")
     company_id = fields.Many2one('res.company', string='company', readonly=True,
                                  default=lambda self: self.env.company.id)
+    is_confirm = fields.Boolean(string='Is confirm')
     tick = fields.Boolean(string="Consider Closing Stock")
 
     @api.model
@@ -31,17 +33,12 @@ class PlanPlaning(models.Model):
 
     def action_line_value(self):
         self.production_lines_ids = False
-        cdtn = '''where so.state = 'sale' and si.expected_date BETWEEN '%s' AND '%s'
-
-                        ''' % (
-        self.planning_date.strftime("%Y-%m-%d 00:00:00"), self.planning_date.strftime("%Y-%m-%d 23:59:59"))
-
         filter_cdtn = '''where si.state = 'confirmed' and si.expected_date BETWEEN '%s' AND '%s'
 
         ''' % (self.planning_date.strftime("%Y-%m-%d 00:00:00"), self.planning_date.strftime("%Y-%m-%d 23:59:59"))
         cdtn = '''where so.state = 'sale' and so.validity_date BETWEEN '%s' AND '%s'
                 ''' % (
-        self.planning_date.strftime("%Y-%m-%d 00:00:00"), self.planning_date.strftime("%Y-%m-%d 23:59:59"))
+            self.planning_date.strftime("%Y-%m-%d 00:00:00"), self.planning_date.strftime("%Y-%m-%d 23:59:59"))
         if self.item_category_id:
             filter_cdtn += ''' and pt.categ_id = %s 
             ''' % (self.item_category_id.id)
@@ -81,7 +78,7 @@ class PlanPlaning(models.Model):
                     LEFT JOIN sale_order so ON sol.order_id = so.id
                     LEFT JOIN uom_uom uom ON sol.product_uom = uom.id
                     LEFT JOIN product_product pp ON sol.product_id = pp.id
-                    LEFT JOIN product_template pt ON pp.product_tmpl_id = pt.id
+					LEFT JOIN product_template pt ON pp.product_tmpl_id = pt.id
                     JOIN mrp_bom bom ON pt.id = bom.product_tmpl_id
                     %s 
                     GROUP BY sol.product_id, pt.categ_id,uom.id,bom.id
@@ -90,72 +87,211 @@ class PlanPlaning(models.Model):
         self._cr.execute(query)
         print(query)
         intent_ids = self._cr.dictfetchall()
-        print(intent_ids, 'pandal')
-        for each in intent_ids:
-            print(each['product'])
-            print(each['total_qty'])
-            print(each['uom_name'])
+        # list1 = []
+        # print(intent_ids, 'pandal')
+        # grouped_data = {}
+        # for item in intent_ids:
+        #     product_id = item['product']
+        #     if product_id in grouped_data:
+        #         grouped_data[product_id]['order_quantity'] += item['total_qty']
+        #     else:
+        #         grouped_data[product_id] = {
+        #             'item_list_id': product_id,
+        #             'order_quantity': item['total_qty'],
+        #             'production_uom_id': item['uom_name'],
+        #             'bom_id': item['bom']
+        #         }
+        #
+        # result = list(grouped_data.values())
+        # list1.append(result)
+
+        grouped_data = {}
+        for item in intent_ids:
+            product_id = item['product']
+            if product_id in grouped_data:
+                grouped_data[product_id]['total_qty'] += item['total_qty']
+            else:
+                grouped_data[product_id] = item
+
+        result = list(grouped_data.values())
+        print(result)
+        for i in result:
             self.write({
                 'production_lines_ids': [(0, 0, {
-                    'item_list_id': each['product'],
-                    'order_quantity': each['total_qty'],
-                    'production_uom_id': each['uom_name'],
-
+                    'item_list_id': i['product'],
+                    'bom_id': i['bom'],
+                    'production_uom_id': i['uom_name'],
+                    'order_quantity': i['total_qty'],
                 })]
             })
+
+        # list = []
+        # print(list)
+        # no_dup = []
+        # no_dup2_list = []
+        # for j in intent_ids:
+        #     # if j not in no_dup:
+        #         no_dup.append(j['product'])
+        # print(set(no_dup))
+        # tot = 0
+        # for i in set(no_dup):
+        #     print(i)
+        #     x = {}
+        #     for pand in intent_ids:
+        #         print(pand)
+        #         if pand['product'] == i:
+        #             # if pand['bom'] == i['bom']:
+        #
+        #             tot += pand['total_qty']
+        #
+        #             x = {
+        #                 'product': pand['product'],
+        #                 'total_quan': tot,
+        #                 'uom': pand['uom_name'],
+        #                 'bom': pand['bom']
+        #             }
+        #     no_dup2_list.append(x)
+        #
+        # print(no_dup2_list, 'okkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk')
+        # for lines in no_dup2_list:
+        #     self.write({
+        #         'production_lines_ids': [(0, 0, {
+        #             'item_list_id': lines['product'],
+        #             'order_quantity': lines['total_quan'],
+        #             'production_uom_id': lines['uom'],
+        #             'bom_id': lines['bom'],
+        #         })]
+        #     })
 
     def action_waiting_approval(self):
         # plan = self.production_lines_ids
         user = self.env.uid
+        # self.state = 'approval'
         date_today = date.today()
         print(date_today)
         if self.planning_date >= date_today:
             self.state = 'approval'
         else:
             raise UserError(_("Planning Date Must Be Greater or Equal To Today Date"))
+
     def action_approve(self):
         user = self.env.uid
         self.state = 'approve'
 
     def action_confirm(self):
         self.state = 'done'
-    #     self.action_create_transfer()
-    # def action_create_transfer(self):
-    #     if self.company_id.production_operation_type:
-    #         if self.company_id.production_operation_type.default_location_src_id and self.company_id.production_operation_type.default_location_dest_id:
-    #             picking_type = self.company_id.production_operation_type
-    #             to_location_id = self.company_id.production_operation_type.default_location_dest_id
-    #             from_location_id = self.company_id.production_operation_type.default_location_src_id
-    #             transfer_move = self.env['stock.picking'].create({
-    #                 # 'partner_id': self.sale_id.partner_id.id,
-    #                 'picking_type_id': picking_type.id,
-    #                 'company_id': self.company_id.id,
-    #                 'origin': "Material Request",
-    #                 # 'project_name': self.project_name_id.id,
-    #                 # 'project_task': self.project_task_id.id,
-    #                 'location_id': from_location_id.id,
-    #                 'location_dest_id': to_location_id.id,
-    #                 'production_id': self.id,
-    #             })
-    #             for vals in self.product_line_ids:
-    #                 transfer_move.sudo().write({
-    #                     'move_ids_without_package': [(0, 0, {
-    #                         'product_id': vals.product_id.id,
-    #                         'name': vals.product_id.name,
-    #                         'product_uom_qty': vals.quantity,
-    #                         'location_id': from_location_id.id,
-    #                         'location_dest_id': to_location_id.id,
-    #                         'product_uom': vals.product_uom_id.id,
-    #                         'company_id': self.company_id.id,
-    #                     })]
-    #                 })
+        planing_cdtn = '''where so.state = 'sale' and so.validity_date BETWEEN '%s' AND '%s'
+    
+                            ''' % (
+            self.planning_date.strftime("%Y-%m-%d 00:00:00"), self.planning_date.strftime("%Y-%m-%d 23:59:59"))
+        ind_cdtn = '''where si.state = 'confirmed' and si.expected_date BETWEEN '%s' AND '%s'
+    
+            ''' % (self.planning_date.strftime("%Y-%m-%d 00:00:00"), self.planning_date.strftime("%Y-%m-%d 23:59:59"))
+        if self.company_id:
+            planing_cdtn += ''' and so.company_id = %s
+                ''' % (self.company_id.id)
+            ind_cdtn += ''' and si.company_id = %s
+                ''' % (self.company_id.id)
+
+        ind = """
+                            SELECT si.id as indent
+                            FROM sales_indent_lines sil
+                            LEFT JOIN sales_indent si ON sil.pur_id = si.id
+                            %s
+                            GROUP BY si.id
+                        """ % (ind_cdtn)
+        self._cr.execute(ind)
+        ind_ids = self._cr.dictfetchall()
+        for ind in ind_ids:
+            indent = self.env['sales.indent'].browse(ind['indent'])
+            indent.write({
+                'is_true': True
+            })
+        planing = """  
+                            SELECT so.id as sale
+                            FROM sale_order_line sol
+                            LEFT JOIN sale_order so ON sol.order_id = so.id
+                            %s
+                            GROUP BY so.id
+                        """ % (planing_cdtn)
+        self._cr.execute(planing)
+        planing_ids = self._cr.dictfetchall()
+        print(planing_ids, 'sucess achieved')
+
+        # Iterate over the selected order IDs and update the values
+        for inv in planing_ids:
+            order = self.env['sale.order'].browse(inv['sale'])
+            order.write({
+                'is_true': True
+            })
+
+        transfer_cdtn = '''where si.state ='approve' and si.planning_date BETWEEN '%s' AND '%s'
+    
+                                    ''' % (
+            self.planning_date.strftime("%Y-%m-%d 00:00:00"), self.planning_date.strftime("%Y-%m-%d 23:59:59"))
+        transfer = """
+                    SELECT ptb.categ_id AS categ,  
+                           mbl.product_id AS product,
+                           uom.id as uom_name,
+                           sum(sil.actual_plan_qty * mbl.product_qty) as total
+                    FROM production_plan_lines sil
+                    LEFT JOIN plan_planing si ON sil.ref_id = si.id
+                    LEFT JOIN product_product pp ON sil.item_list_id = pp.id
+                    LEFT JOIN product_template pt ON pp.product_tmpl_id = pt.id
+                    JOIN mrp_bom bom ON pt.id = bom.product_tmpl_id
+                    INNER JOIN mrp_bom_line mbl ON bom.id = mbl.bom_id
+                    LEFT JOIN product_product pn ON mbl.product_id = Pn.id
+                    LEFT JOIN product_template ptb ON mbl.product_tmpl_id = ptb.id
+                    LEFT JOIN uom_uom uom ON mbl.product_uom_id = uom.id
+                    %s
+                    GROUP BY mbl.product_id, ptb.categ_id,uom.id
+                """ % (transfer_cdtn)
+        self._cr.execute(transfer)
+        transfer_ids = self._cr.dictfetchall()
+        print(transfer_ids, 'pandal')
+        if self.company_id.production_picking_type_id:
+            if self.company_id.production_picking_type_id.default_location_dest_id:
+                if self.company_id.production_picking_type_id.default_location_src_id:
+                    picking_type = self.company_id.production_picking_type_id
+                    to_location_id = self.company_id.production_picking_type_id.default_location_dest_id
+                    from_location_id = self.company_id.production_picking_type_id.default_location_src_id
+                    transfer_move = self.env['stock.picking'].create({
+                        'picking_type_id': picking_type.id,
+                        'company_id': self.company_id.id,
+                        'origin': "Material Request",
+                        'location_id': from_location_id.id,
+                        'location_dest_id': to_location_id.id,
+                        'request_id': self.id,
+                    })
+                    for vals in transfer_ids:
+                        transfer_move.sudo().write({
+                            'move_ids_without_package': [(0, 0, {
+                                'product_id': vals['product'],
+                                'name': vals['product'],
+                                'product_uom_qty': vals['total'],
+                                'location_id': from_location_id.id,
+                                'location_dest_id': to_location_id.id,
+                                'product_uom': vals['uom_name'],
+                                'company_id': self.company_id.id,
+                            })]
+                        })
+
     def action_reject(self):
         if self.reason:
-           self.state = 'reject'
+            self.state = 'reject'
 
     @api.onchange('tick')
     def _onchange_tick(self):
         self.production_lines_ids._compute_plan_qty()
+
+    def action_open_purchase_transfer(self):
+        return {
+            'name': _('Transfer'),
+            'type': 'ir.actions.act_window',
+            'res_model': 'stock.picking',
+            'view_mode': 'tree,form',
+            'domain': [('request_id', '=', self.id)],
+        }
 
 
 class ProductionPlanningLines(models.Model):
@@ -163,13 +299,14 @@ class ProductionPlanningLines(models.Model):
     _order = 'item_list_id ASC'
 
     item_list_id = fields.Many2one('product.product', string="Products")
+    bom_id = fields.Many2one('mrp.bom', string="Bom")
     order_quantity = fields.Float(string="Total Order Qty")
-    planed_quantity = fields.Float(string="Plan Qty",compute='_compute_plan_qty', store=True)
-    on_quantity = fields.Float(string="Closing Stock",related="item_list_id.product_tmpl_id.qty_available")
+    planed_quantity = fields.Float(string="Plan Qty", compute='_compute_plan_qty', store=True)
+    on_quantity = fields.Float(string="Closing Stock", related="item_list_id.product_tmpl_id.qty_available")
     production_uom_id = fields.Many2one('uom.uom', string="Unit")
     ref_id = fields.Many2one('plan.planing')
     varience = fields.Float(string="Varience")
-    actual_plan_qty = fields.Float(string="Actual Plan Qty", compute='_compute_actual_plan_qty',store=True)
+    actual_plan_qty = fields.Float(string="Actual Plan Qty", compute='_compute_actual_plan_qty', store=True)
 
     @api.depends('order_quantity', 'on_quantity')
     def _compute_plan_qty(self):
@@ -212,7 +349,15 @@ class ProductionPlanningLines(models.Model):
                 elif sel.varience == 0:
                     sel.actual_plan_qty = sel.planed_quantity
 
-# class ResCompany(models.Model):
-#      _inherit = 'res.company'
-#
-#      production_picking_id = fields.Many2one('')
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    production_picking_type_id = fields.Many2one('stock.picking.type',
+                                                 string='Picking Type For Material From Production')
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    request_id = fields.Many2one('plan.planing', string='Material Request For Production')
