0c879045f9fb98cc7c8eef3095fb95834d2e6ce0
r.arun@catalistindia.in <r.arun@catalistindia.in>
2023-08-08 16:26:42 +0530

    indent

---
+++ sales_indent_report/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Sale Indent Report',
+    'version': '1.0',
+    'summary': 'Sale Indent Report',
+    'sequence': -100,
+    'description': """Sale Indent Report""",
+    'category': 'productivity',
+    'website': 'https://www.odoo.com/app/invoicing',
+    'license': 'LGPL-3',
+    'depends': ['base', 'stock', 'indent','sale', 'purchase'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/sales_indent_report_wizard.xml',
+        'report/report.xml',
+        'report/sales_indent_report.xml',
+        # 'views/sales_indent_menu.xml',
+
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
